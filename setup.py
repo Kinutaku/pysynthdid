baa4b86e3b0edba029f46128b019b24036988ff8
Kinutaku <kinutaku24@gmail.com>
2025-01-01 15:17:59 +0900

    Update setup.py
    - sklearn
    + scikit-learn

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ install_requires = [
     'matplotlib',
     'numpy',
     'tqdm',
-    'sklearn',
+    # 'sklearn',
+    'scikit-learn',
     'scipy',
     'toolz',
     'bayesian-optimization >= 1.1.0'
